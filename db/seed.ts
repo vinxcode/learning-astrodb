0e4a57fdb45cfd758dded084e4a7ab7c7f56104c
Kevin Perdomo <pg1807@ues.edu.sv>
2024-05-02 08:21:32 -0600

    Creating ./.github/workflow/_studio.yaml

--- db/seed.ts
+++ db/seed.ts
@@ -1,9 +1,8 @@
 import { db, User } from 'astro:db'
 
-export default async function seed() {
-	await db.insert(User).values([
-		{ userId: '1', name: "Eduardo", phone: "78787878"},
-		{ userId: '2', name: "Marvin", phone: "71898078"}
-	]);
-	
-}
+ export default async function seed() {
+	// await db.insert(User).values([
+		// { userId: '1', name: "Eduardo", phone: "78787878"},
+		// { userId: '2', name: "Marvin", phone: "71898078"}
+	// ]);
+	 }
